cd2433dc63af5670c4815565282a9a85f2479783
Ivan Ovdiienko <johnny.unerty@gmail.com>
2021-10-19 19:02:33 +0300

    feat: add release an publish options

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,14 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, NodePackageManager } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Ivan Ovdiienko',
-  authorAddress: 'johnny.unerty@gmail.com',
-  cdkVersion: '1.95.2',
+  authorAddress: 'ivan.ovdiienko@pepperize.com',
+  authorOrganization: "Pepperize",
+  cdkVersion: '1.128.0',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   name: 'cdk-gitlab-runner',
   repositoryUrl: 'https://github.com/pepperize/cdk-gitlab-runner.git',
+  packageManager: NodePackageManager.NPM,
 
   // cdkDependencies: undefined,      /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,  /* AWS CDK modules required for testing. */
@@ -13,6 +16,17 @@ const project = new AwsCdkConstructLibrary({
   // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                     /* Build dependencies for this module. */
   // packageName: undefined,          /* The "name" in package.json. */
-  // release: undefined,              /* Add release management to this project. */
+  release: {
+    releaseEveryCommit: true,
+  },
+  releaseToNpm: {
+    distTag: 'latest',
+    npmTokenSecret: 'NPM_TOKEN', // Default: "NPM_TOKEN" or "GITHUB_TOKEN" if registry is set to npm.pkg.github.com.
+    registry: 'registry.npmjs.org',
+  },
+  publishToNuget: {},
+  publishToPypi: {},
+  publishToMaven: {},
+  publishToGo: {},
 });
 project.synth();
