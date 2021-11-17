e797d2608745c64558da9dbd7f9503e5bba2026c
Ivan Ovdiienko <johnny.unerty@gmail.com>
2021-11-17 19:25:48 +0200

    chore: add more cache tests

--- test/runner/cache.test.ts
+++ test/runner/cache.test.ts
@@ -5,19 +5,44 @@ import {
 } from "../../src/runner/cache";
 
 const mockApp = new App();
-const mockStack = new Stack(mockApp, "test-stack", {
+const stackProps = {
   env: {
     account: "0",
     region: "us-east-1",
   },
-});
+};
 
-test("cache", () => {
+test("cache-expiration-set-to-0", () => {
+  const mockStack = new Stack(mockApp, "test-stack-0", stackProps);
   const props: CacheConstructProps = {
-    bucketName: "test",
+    bucketName: "test-0",
     expiration: Duration.days(0),
   };
-  const cache = new CacheConstruct(mockStack, "test", props);
+  const cache = new CacheConstruct(mockStack, "test-0", props);
 
   expect(cache.expiration.toDays()).toBe(Duration.days(0).toDays());
+  expect(cache.lifeCycleRuleEnabled).toBe(false);
+});
+
+test("cache-expiration-not-set-and-becomes-default", () => {
+  const mockStack = new Stack(mockApp, "test-stack-undefined", stackProps);
+  const props: CacheConstructProps = {
+    bucketName: "test-undefined",
+  };
+  const cache = new CacheConstruct(mockStack, "test-undefined", props);
+
+  expect(cache.expiration.toDays()).toBe(Duration.days(30).toDays());
+  expect(cache.lifeCycleRuleEnabled).toBe(true);
+});
+
+test("cache-expiration-set-to-value-1", () => {
+  const mockStack = new Stack(mockApp, "test-stack-set-to-value-1", stackProps);
+  const props: CacheConstructProps = {
+    bucketName: "test-set-to-value-1",
+    expiration: Duration.days(1),
+  };
+  const cache = new CacheConstruct(mockStack, "test-set-to-value-1", props);
+
+  expect(cache.expiration.toDays()).toBe(Duration.days(1).toDays());
+  expect(cache.lifeCycleRuleEnabled).toBe(true);
 });
