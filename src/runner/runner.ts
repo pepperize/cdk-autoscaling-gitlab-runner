634d7af189e31412d2a0d30302e76cb18d1a2993
Patrick Florek <patrick.florek@gmail.com>
2021-11-24 19:41:05 +0100

    feat: using latest amzn 2 image

--- src/runner/runner.ts
+++ src/runner/runner.ts
@@ -4,6 +4,11 @@ import {
   Signals,
 } from "@aws-cdk/aws-autoscaling";
 import {
+  AmazonLinuxCpuType,
+  AmazonLinuxEdition,
+  AmazonLinuxGeneration,
+  AmazonLinuxStorage,
+  AmazonLinuxVirt,
   CloudFormationInit,
   IMachineImage,
   InitCommand,
@@ -49,28 +54,6 @@ import { Network, NetworkProps } from "./network";
  *
  * @packageDocumentation
  */
-
-/**
- * A record of Region: string and AmiID: string
- */
-export const managerAmiMap: Record<string, string> = {
-  "eu-north-1": "ami-d16fe6af",
-  "ap-south-1": "ami-0889b8a448de4fc44",
-  "eu-west-3": "ami-0451ae4fd8dd178f7",
-  "eu-west-2": "ami-09ead922c1dad67e4",
-  "eu-west-1": "ami-07683a44e80cd32c5",
-  "ap-northeast-2": "ami-047f7b46bd6dd5d84",
-  "ap-northeast-1": "ami-0f9ae750e8274075b",
-  "sa-east-1": "ami-0669a96e355eac82f",
-  "ca-central-1": "ami-03338e1f67dae0168",
-  "ap-southeast-1": "ami-0b419c3a4b01d1859",
-  "ap-southeast-2": "ami-04481c741a0311bbb",
-  "eu-central-1": "ami-09def150731bdbcc2",
-  "us-east-1": "ami-0de53d8956e8dcf80",
-  "us-east-2": "ami-02bcbb802e03574ba",
-  "us-west-1": "ami-0019ef04ac50be30f",
-  "us-west-2": "ami-061392db613a6357b",
-};
 
 /**
  * Properties of the Gitlab Runner. You have to provide at least the GitLab's Runner's authentication token.
@@ -316,7 +299,14 @@ export class GitlabRunnerAutoscaling extends Construct {
       InstanceType.of(InstanceClass.T3, InstanceSize.NANO);
 
     const managerMachineImage =
-      runners?.machineImage || MachineImage.genericLinux(managerAmiMap);
+      runners?.machineImage ||
+      MachineImage.latestAmazonLinux({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+        edition: AmazonLinuxEdition.STANDARD,
+        virtualization: AmazonLinuxVirt.HVM,
+        storage: AmazonLinuxStorage.EBS,
+        cpuType: AmazonLinuxCpuType.X86_64,
+      });
 
     const managerRole = new Role(scope, "ManagerRole", {
       assumedBy: ec2ServicePrincipal,
