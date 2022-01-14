3754e2e900e198fe9b5e5f46e0f8d474e9df783f
Ivan Ovdiienko <johnny.unerty@gmail.com>
2022-01-14 16:05:49 +0200

    chore: move manager props to a separate file

--- src/runner/runner.ts
+++ src/runner/runner.ts
@@ -34,6 +34,7 @@ import {
   MachineConfiguration,
 } from "../runner-configuration";
 import { Cache, CacheProps } from "./cache";
+import { GitlabRunnerAutoscalingManagerProps } from "./manager";
 import { Network, NetworkProps } from "./network";
 
 /**
@@ -81,29 +82,6 @@ export interface GitlabRunnerAutoscalingCacheProps {
    * If no existing S3 bucket is provided, a S3 bucket will be created.
    */
   readonly options?: CacheProps;
-}
-
-export interface GitlabRunnerAutoscalingManagerProps {
-  /**
-   * An Amazon Machine Image ID for the Manager EC2 instance. If empty the latest Amazon 2 Image will be looked up.
-   *
-   * Should be RHEL flavor like Amazon Linux 2 with yum available for instance initialization.
-   *
-   * @see https://cloudinit.readthedocs.io/en/latest/
-   * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-init.html
-   */
-  readonly machineImage?: IMachineImage;
-
-  /**
-   * Instance type for manager EC2 instance. It's a combination of a class and size.
-   * @default InstanceType.of(InstanceClass.T3, InstanceSize.NANO)
-   */
-  readonly instanceType?: InstanceType;
-
-  /**
-   * A set of security credentials that you use to prove your identity when connecting to an Amazon EC2 instance. You won't be able to ssh into an instance without the Key Pair.
-   */
-  readonly keyPairName?: string;
 }
 
 /**
