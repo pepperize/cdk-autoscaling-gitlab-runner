6381ba8a58aadfbc7f892febfbb3f8d47148309a
Ivan Ovdiienko <johnny.unerty@gmail.com>
2022-01-14 16:59:21 +0200

    feat(manager): set up basic manager props

--- src/runner/manager.ts
+++ src/runner/manager.ts
@@ -1,4 +1,16 @@
-import { IMachineImage, InstanceType } from "@aws-cdk/aws-ec2";
+import {
+  AmazonLinuxCpuType,
+  AmazonLinuxEdition,
+  AmazonLinuxGeneration,
+  AmazonLinuxStorage,
+  AmazonLinuxVirt,
+  IMachineImage,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  MachineImage,
+} from "@aws-cdk/aws-ec2";
+import { Construct, Stack } from "@aws-cdk/core";
 
 export interface GitlabRunnerAutoscalingManagerProps {
   /**
@@ -21,4 +33,30 @@ export interface GitlabRunnerAutoscalingManagerProps {
    * A set of security credentials that you use to prove your identity when connecting to an Amazon EC2 instance. You won't be able to ssh into an instance without the Key Pair.
    */
   readonly keyPairName?: string;
+}
+
+/**
+ * Settings for the manager (coordinator)
+ *
+ *  Manager coordinates the placement of runner (job executor) instances
+ */
+export class GitlabRunnerAutoscalingManager extends Construct {
+  readonly machineImage: IMachineImage;
+  readonly instanceType: InstanceType;
+  readonly keyPairName?: string;
+
+  constructor(scope: Stack, id: string, props?: GitlabRunnerAutoscalingManagerProps) {
+    super(scope, id);
+    this.machineImage =
+      props?.machineImage ??
+      MachineImage.latestAmazonLinux({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+        edition: AmazonLinuxEdition.STANDARD,
+        virtualization: AmazonLinuxVirt.HVM,
+        storage: AmazonLinuxStorage.EBS,
+        cpuType: AmazonLinuxCpuType.X86_64,
+      });
+    this.instanceType = props?.instanceType ?? InstanceType.of(InstanceClass.T3, InstanceSize.NANO);
+    this.keyPairName = props?.keyPairName;
+  }
 }
