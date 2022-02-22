cfccaf4b879cfdcbbf3e80ab1fe89e55bb3e75b9
Ivan Ovdiienko <johnny.unerty@gmail.com>
2022-02-22 17:28:23 +0200

    chore: remove the old test

--- test/runner/network.test.ts
+++ test/runner/network.test.ts
@@ -52,17 +52,3 @@ describe("Network", () => {
     );
   });
 });
-
-test("network", () => {
-  const app = new App();
-  const stack = new Stack(app, `stack`);
-
-  const network = new Network(stack, `network`);
-
-  expect(network.availabilityZone).toBeDefined();
-  expect(network.subnet.subnetId).toBeDefined();
-  expect(network.vpc.vpcId).toBeDefined();
-  expect(network.availabilityZone).toEqual(network.vpc.availabilityZones[0]);
-  expect(network.availabilityZone).toEqual(network.subnet.availabilityZone);
-  expect(network.vpc.availabilityZones[0]).toEqual(network.subnet.availabilityZone);
-});
